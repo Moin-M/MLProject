360526527fc6fc4a1e5d434b3937c4a3b33608b9
Moin-M <moinudhinhasan@gmail.com>
2023-04-27 11:58:43 +0530

    model trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -14,6 +14,7 @@ import os
 
 from src.utils import save_object
 
+
 @dataclass
 class DataTransformationConfig:
     preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
@@ -120,5 +121,6 @@ class DataTransformation:
                 test_arr,
                 self.data_transformation_config.preprocessor_obj_file_path,
             )
+        
         except Exception as e:
             raise CustomException(e,sys)
